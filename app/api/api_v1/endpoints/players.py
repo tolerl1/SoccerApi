f80329ffb947e2144211bdcf8398d2e51032a17a
logan <logan.toler@live.com>
2022-11-21 03:49:56 -0500

    get multi players

--- app/api/api_v1/endpoints/players.py
+++ app/api/api_v1/endpoints/players.py
@@ -1,3 +1,4 @@
+from typing import Optional, Union
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import crud, models, schemas
@@ -7,10 +8,19 @@ from app.api import deps
 router = APIRouter()
 
 
+@router.get("/", response_model=list[schemas.Player])
+def read_players(db: Session = Depends(deps.get_db), skip: int = 0, limit: Union[int, None] = None):
+    """Retrieve list of players."""
+    db_player = crud.get_multi_player(db, skip=skip, limit=limit)
+    if db_player is None:
+        raise HTTPException(status_code=404, detail="some error")
+    return db_player
+
+
 @router.get("/{player_id}", response_model=schemas.Player)
-def read_player(player_id: int, db: Session = Depends(deps.get_db)):
-    """Retrieve a player."""
-    db_player = crud.get_player(db, player_id=player_id)
+def read_player_by_id(player_id: int, db: Session = Depends(deps.get_db)):
+    """Retrieve a player by id."""
+    db_player = crud.get_player_by_id(db, player_id=player_id)
     if db_player is None:
         raise HTTPException(status_code=404, detail="Player not found")
     return db_player
--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,8 @@
+from typing import Optional, Union
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from app import models
+from app.schemas import Player
 
 
 def get_club(db: Session, club_id: int):
@@ -15,5 +17,14 @@ def get_game(db: Session, game_id: int):
     return db.query(models.Game).filter(models.Game.game_id == game_id).first()
 
 
-def get_player(db: Session, player_id: int):
+def get_player_by_id(db: Session, player_id: int):
     return db.query(models.Player).filter(models.Player.player_id == player_id).first()
+
+
+def get_multi_player(db: Session, skip: int = 0, limit: Union[int, None] = None) -> list[Player]:
+    return (
+        db.query(models.Player)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
